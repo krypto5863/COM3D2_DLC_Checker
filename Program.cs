5e3b906d8236ca7bddd53c854570f53eb79cd2d1
krypto5863 <29824718+krypto5863@users.noreply.github.com>
2024-05-05 09:58:06 -0400

    Fixed issue of a registry path pointing to an invalid directory causing a crash.

--- Program.cs
+++ Program.cs
@@ -23,7 +23,7 @@ namespace COM3D2_DLC_Checker
             // HTTP_RESOPOND
             //  - Item1 = HTTP Status Code
             //  - Item2 = Internet DLC List content
-            Tuple<HttpStatusCode, string> HTTP_RESPOND = CONNECT_TO_INTERNET(DLC_URL);
+            var HTTP_RESPOND = CONNECT_TO_INTERNET(DLC_URL);
 
             if (HTTP_RESPOND.Item1 == HttpStatusCode.OK)
             {
@@ -36,13 +36,13 @@ namespace COM3D2_DLC_Checker
             }
 
             // DLC LIST = [DLC_FILENAME, DLC_NAME]
-            IDictionary<string, string> DLC_LIST = READ_DLC_LIST();
-            List<string> GAMEDATA_LIST = READ_GAMEDATA();
+            var DLC_LIST = READ_DLC_LIST();
+            var GAMEDATA_LIST = READ_GAMEDATA();
 
             // DLC LIST SORTED
             // Item 1 = INSTALLED_DLC
             // Item 2 = NOT_INSTALLED_DLC
-            Tuple<List<string>, List<string>> DLC_LIST_SORTED = COMPARE_DLC(DLC_LIST, GAMEDATA_LIST);
+            var DLC_LIST_SORTED = COMPARE_DLC(DLC_LIST, GAMEDATA_LIST);
 
             PRINT_DLC(DLC_LIST_SORTED.Item1, DLC_LIST_SORTED.Item2);
 
@@ -58,15 +58,15 @@ namespace COM3D2_DLC_Checker
 
         static Tuple<HttpStatusCode, string> CONNECT_TO_INTERNET(string DLC_URL)
         {
-            HttpWebRequest httpWebRequest = (HttpWebRequest)WebRequest.Create(DLC_URL);
-            HttpWebRequest request = httpWebRequest;
+            var httpWebRequest = (HttpWebRequest)WebRequest.Create(DLC_URL);
+            var request = httpWebRequest;
             request.AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate;
 
             try
             {
-                using HttpWebResponse response = (HttpWebResponse)request.GetResponse();
-                using Stream stream = response.GetResponseStream();
-                using StreamReader reader = new StreamReader(stream);
+                using var response = (HttpWebResponse)request.GetResponse();
+                using var stream = response.GetResponseStream();
+                using var reader = new StreamReader(stream);
 
                 return new Tuple<HttpStatusCode, string>(response.StatusCode, reader.ReadToEnd());
             }
@@ -78,13 +78,13 @@ namespace COM3D2_DLC_Checker
 
         static void UPDATE_DLC_LIST(string UPDATED_CONTENT)
         {
-            using StreamWriter writer = new StreamWriter(DLC_LIST_PATH);
+            using var writer = new StreamWriter(DLC_LIST_PATH);
             writer.Write(UPDATED_CONTENT);
         }
 
         static IDictionary<string, string> READ_DLC_LIST()
         {
-            List<string> DLC_LIST_UNFORMATED = new List<string>();
+            var DLC_LIST_UNFORMATED = new List<string>();
 
             try
             {
@@ -103,9 +103,9 @@ namespace COM3D2_DLC_Checker
             // DLC_LIST_FORMAT = [Keys = DLC_Filename, Value = DLC_Name]
             IDictionary<string, string> DLC_LIST_FORMATED = new Dictionary<string, string>();
 
-            foreach (string DLC_LIST in DLC_LIST_UNFORMATED)
+            foreach (var DLC_LIST in DLC_LIST_UNFORMATED)
             {
-                String[] temp_strlist = DLC_LIST.Split(',');
+                var temp_strlist = DLC_LIST.Split(',');
                 DLC_LIST_FORMATED.Add(temp_strlist[0], temp_strlist[1]);
             }
 
@@ -119,26 +119,24 @@ namespace COM3D2_DLC_Checker
             // Will replaced by COM3D2 InstallPath Registry
             const string keyName = "HKEY_CURRENT_USER" + "\\" + "SOFTWARE\\KISS\\カスタムオーダーメイド3D2";
 
-            string GAME_DIRECTORY_REGISTRY = (string)Registry.GetValue(keyName,"InstallPath","");
+            var GAME_DIRECTORY_REGISTRY = (string)Registry.GetValue(keyName,"InstallPath","");
 
-            if (GAME_DIRECTORY_REGISTRY != null)
+            if (GAME_DIRECTORY_REGISTRY != null && Directory.Exists(GAME_DIRECTORY_REGISTRY) && File.Exists(Path.Combine(GAME_DIRECTORY_REGISTRY, "COM3D2x64.exe")))
             {
                 return GAME_DIRECTORY_REGISTRY;
             }
-            else
-            {
-                CONSOLE_COLOR(ConsoleColor.Yellow, "Warning : COM3D2 installation directory is not set in registry. Will using work directory', 'yellow'");
-                return Directory.GetCurrentDirectory();
-            }
+
+            CONSOLE_COLOR(ConsoleColor.Yellow, "Warning : COM3D2 installation directory is not set in registry. Will use working directory', 'yellow'");
+            return Directory.GetCurrentDirectory();
         }
 
         static List<string> READ_GAMEDATA()
         {
-            string GAME_DIRECTORY = GET_COM3D2_INSTALLPATH();
-            string GAMEDATA_DIRECTORY = GAME_DIRECTORY + "\\GameData";
-            string GAMEDATA_20_DIRECTORY = GAME_DIRECTORY + "\\GameData_20";
+            var GAME_DIRECTORY = GET_COM3D2_INSTALLPATH();
+            var GAMEDATA_DIRECTORY = GAME_DIRECTORY + "\\GameData";
+            var GAMEDATA_20_DIRECTORY = GAME_DIRECTORY + "\\GameData_20";
 
-            List<string> GAMEDATA_LIST = new List<string>();
+            var GAMEDATA_LIST = new List<string>();
 
             GAMEDATA_LIST.AddRange(Directory.GetFiles(@GAMEDATA_DIRECTORY, "*", SearchOption.TopDirectoryOnly).Select(Path.GetFileName));
             GAMEDATA_LIST.AddRange(Directory.GetFiles(@GAMEDATA_20_DIRECTORY, "*", SearchOption.TopDirectoryOnly).Select(Path.GetFileName));
@@ -149,14 +147,14 @@ namespace COM3D2_DLC_Checker
         static Tuple<List<string>,List<string>> COMPARE_DLC(IDictionary<string, string> DLC_LIST, List<string> GAMEDATA_LIST)
         {
             // DLC LIST = [DLC_FILENAME, DLC_NAME]
-            List<string> DLC_FILENAMES = new List<string>(DLC_LIST.Keys);
-            List<string> DLC_NAMES= new List<string>(DLC_LIST.Values);
+            var DLC_FILENAMES = new List<string>(DLC_LIST.Keys);
+            var DLC_NAMES= new List<string>(DLC_LIST.Values);
 
-            List<string> INSTALLED_DLC = new List<string>(); 
-            foreach(string INSTALLED_DLC_FILENAMES in DLC_FILENAMES.Intersect(GAMEDATA_LIST).ToList())
+            var INSTALLED_DLC = new List<string>(); 
+            foreach(var INSTALLED_DLC_FILENAMES in DLC_FILENAMES.Intersect(GAMEDATA_LIST).ToList())
             {
                 // UNIT_DLC_LIST = [DLC_FILENAME, DLC_NAME]
-                foreach (KeyValuePair<string,string> UNIT_DLC_LIST in DLC_LIST)
+                foreach (var UNIT_DLC_LIST in DLC_LIST)
                 {
                     if (INSTALLED_DLC_FILENAMES == UNIT_DLC_LIST.Key)
                     {
@@ -167,7 +165,7 @@ namespace COM3D2_DLC_Checker
                 }
             }
             
-            List<string> NOT_INSTALLED_DLC = DLC_NAMES.Except(INSTALLED_DLC).ToList();
+            var NOT_INSTALLED_DLC = DLC_NAMES.Except(INSTALLED_DLC).ToList();
             INSTALLED_DLC.Sort();
             NOT_INSTALLED_DLC.Sort();
             return Tuple.Create(INSTALLED_DLC, NOT_INSTALLED_DLC);
@@ -176,13 +174,13 @@ namespace COM3D2_DLC_Checker
         static void PRINT_DLC(List<string> INSTALLED_DLC, List<string> NOT_INSTALLED_DLC)
         {
             CONSOLE_COLOR(ConsoleColor.Cyan, "\nAlready Installed:");
-            foreach (string DLC in INSTALLED_DLC)
+            foreach (var DLC in INSTALLED_DLC)
             {
                 Console.WriteLine(DLC);
             }
 
             CONSOLE_COLOR(ConsoleColor.Cyan, "\nNot Installed :");
-            foreach (string DLC in NOT_INSTALLED_DLC)
+            foreach (var DLC in NOT_INSTALLED_DLC)
             {
                 Console.WriteLine(DLC);
             }
